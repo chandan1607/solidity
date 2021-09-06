4b17f62c24335beb9d48c508589c73eb2e4d2063
chandan1607 <71363385+chandan1607@users.noreply.github.com>
2021-09-06 13:49:44 +0530

    with owner modifier

---
+++ owner.sol
@@ -0,0 +1,33 @@
+contract smart{
+    uint256 public count = 0;
+    mapping(uint => person) public people;
+    
+    address owner;
+    
+    modifier onlyOwner () {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor () public {
+        owner = msg.sender;
+    }
+    
+    struct person{
+        uint id;
+        string firstName;
+        string lastName;
+        
+    }
+    function addPerson(string memory firstName , string memory lastName) public onlyOwner{
+        increment();
+        people[count] =person(count,firstName,lastName);
+        
+    }
+    
+    function increment () internal {
+        count+=1;
+        
+        
+    }
+}
