9d2f87f68603aa43db76f38bd6946968c1672386
chandan1607 <71363385+chandan1607@users.noreply.github.com>
2021-09-06 15:04:14 +0530

    Create people2.sol

---
+++ people2.sol
@@ -0,0 +1,17 @@
+//key value mapping 
+contract smart{
+    uint256 public count = 0;
+    mapping(uint => person) public people;
+    
+    struct person{
+        uint id;
+        string firstName;
+        string lastName;
+        
+    }
+    function addPerson(string memory firstName , string memory lastName) public{
+        count+=1;
+        people[count] =person(count,firstName,lastName);
+        
+    }
+}
