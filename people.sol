f3686bf631f1028c2c6d1c7a0d862ac417e41293
chandan1607 <71363385+chandan1607@users.noreply.github.com>
2021-09-06 11:22:42 +0530

    Create people.sol

---
+++ people.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.5.1;
+
+contract smart {
+    person[] public people;
+    uint256 public count;
+    struct person {
+        string firstName;
+        string lastName;
+        
+    }
+    function addPerson( string memory firstName, string memory lastName) public  {
+        
+        people.push(person(firstName,lastName));
+        count += 1;
+        
+    }
+}
